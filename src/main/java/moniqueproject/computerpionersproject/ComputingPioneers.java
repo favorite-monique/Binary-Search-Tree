4794471d07f6eb028bd31e57f2d8e0e2e414dee8
Katrina Tester <katrin.hartmann@gmail.com>
2021-07-15 00:24:03 +0100

    added path to data file

--- src/main/java/moniqueproject/computerpionersproject/ComputingPioneers.java
+++ src/main/java/moniqueproject/computerpionersproject/ComputingPioneers.java
@@ -6,6 +6,7 @@
 package moniqueproject.computerpionersproject;
 
 import java.io.BufferedReader;
+import java.io.Reader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -16,56 +17,57 @@ import javax.xml.crypto.Data;
  * @author 44773
  */
 public class ComputingPioneers {
-    
+
     private String firstName;
     private String lastName;
     private String invention;
     int n = 14;
-    
-    public ComputingPioneers(String firstName, String lastName, String invention){
+
+    public ComputingPioneers(String firstName, String lastName, String invention) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.invention = invention;
     }
-    
-    public String getfirstName(){
+
+    public String getfirstName() {
         return firstName;
     }
+
     public String getlastName() {
         return lastName;
     }
+
     public String getinvention() {
         return invention;
     }
-    
-public  static void main(String[] args) throws IOException {
-    System.out.println("ComputerPioneers.txt");
-    
-    //Stack stack = new Stack();
-    //Queues queue = new Queues();
 
-    File stackfile = new File("ComputerPioneers.txt");
+    public static void main(String[] args) throws IOException {
 
-    if (!stackfile.exists()) {
-        stackfile.createNewFile();
-        System.out.println("File does not exist");
-    } else {
-        System.out.println("File is done");
-    }
-    FileReader r = new FileReader(stackfile);
-    BufferedReader reader = new BufferedReader(r);
+        //Stack stack = new Stack();
+        //Queues queue = new Queues();
+        //at the end decide where the file to be read should be located
+        File stackfile = new File("./src/main/java/moniqueproject/computerpionersproject/ComputerPioneers.txt");
 
-    String s;
+        if (!stackfile.exists()) {
+            stackfile.createNewFile();
+            System.out.println("File does not exist");
+        } else {
+            System.out.println("File is done");
+        }
 
-    // First, split to be converted
-    //reader.readLine();
+        BufferedReader reader = new BufferedReader(new FileReader(stackfile));
 
-    while ((s = reader.readLine()) != null) {
-        //System.out.println(lin);
-        System.out.println("Reading lines");
-    }
+        String s = null;
 
-       /* String[] splitLine = line.trim().split(";");
+        int counter = 1;
+        while ((s = reader.readLine()) != null) {
+            System.out.println(counter + ": " + s);
+            counter++;
+        }
+
+        //System.out.println(s = reader.readLine());
+
+        /* String[] splitLine = line.trim().split(";");
 
         if (splitLine.length == 2)
             stack.Push(new Data(splitLine[0], splitLine[1]));
@@ -76,9 +78,9 @@ public  static void main(String[] args) throws IOException {
             obj1.convertToDec(n);
             obj1.convertToChar(n);
     }*/
-    reader.close();
+        reader.close();
 
-    /*File queuefile = new File("queue.txt");
+        /*File queuefile = new File("queue.txt");
 
     if (!queuefile.exists()) {
         queuefile.createNewFile();
@@ -99,12 +101,9 @@ public  static void main(String[] args) throws IOException {
         if (splitLine.length == 3)
             queue.insert(new Data(splitLine[0], splitLine[1], splitLine[2]));
     }
+        
+        reader.close();
+         */
+    }
 
-    read.close();
-*/
-}
-    
-    
-    
-    
 }
