0485d065b564ee0fe5181779734f7efde199f0b5
favorite-monique <76563464+favorite-monique@users.noreply.github.com>
2021-07-15 14:33:55 +0100

    Check the queue codes

--- src/main/java/moniqueproject/computerpionersproject/ComputingPioneers.java
+++ src/main/java/moniqueproject/computerpionersproject/ComputingPioneers.java
@@ -32,56 +32,112 @@ public class ComputingPioneers {
     private String lastName;
     private String invention;
     int n = 14;
-    
+    //build a model constructor 
+
+    /**
+     *
+     * @param firstName
+     * @param lastName
+     * @param invention
+     */
     public ComputingPioneers(String firstName, String lastName, String invention){
         this.firstName = firstName;
         this.lastName = lastName;
         this.invention = invention;
     }
-    
+    //get
+
+    /**
+     *
+     * @return
+     */
     public String getfirstName(){
         return firstName;
     }
+
+    /**
+     *
+     * @return
+     */
     public String getlastName() {
         return lastName;
     }
+
+    /**
+     *
+     * @return
+     */
     public String getinvention() {
         return invention;
     }
     
-public static void main(String[] args)throws Exception
+    /**
+     *
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args)throws Exception 
   {
-    System.out.println("./src/main/java/moniqueproject/computerpioners/ComputerPioneers.txt");
-    
-    //Stack stack = new Stack();
-    //Queues queue = new Queues();
-
-    File file = new File("./src/main/java/moniqueproject/computerpioners/ComputerPioneers.txt");
-
-    /*if (!stackfile.exists()) {
-        stackfile.createNewFile();
-        System.out.println("File does not exist");
-    } else {
-        System.out.println("File is done");
-    }*/
-    FileReader r = new FileReader(file);
-    BufferedReader reader = new BufferedReader(r);
-
-    String s = null, line = null;
-
-    // First, split to be converted
-    //reader.readLine();
-    //System.out.println("line is: " + reader.readLine());
-    int counter = 1;
-    while ((line = reader.readLine()) != null) {
-        s += line;
-        counter++;
-        System.out.println(s);
-        System.out.println(counter + s + " ");
-    }
-   
+      System.out.println("./src/main/java/moniqueproject/computerpionersproject/ComputerPioneers.txt");
+      File stackfile = new File("./src/main/java/moniqueproject/computerpionersproject/ComputerPioneers.txt");
+      if(!stackfile.exists()) {
+          stackfile.createNewFile();
+          System.out.println("File does not exist");
+      } else {
+          System.out.println("File is done");
+      }
+      FileReader r = new FileReader(stackfile);
+      BufferedReader reader = new BufferedReader(r);
+      
+      String s, line = null;
+      int counter = 1;
+      while((s = reader.readLine()) !=null) {
+          counter++;
+          System.out.println(counter + "." + s);
+      }
   }
 
+    /**
+     *
+     * @param s
+     * @return
+     */
+    public static String[] getWords(String s) {
+          s = s.replaceAll("\\(0.9", "");
+          s = s.replaceAll("\\s+", "");
+          s = s.replaceAll("\\p(Punct)", "");
+          String[] values;
+          String line = null;
+          values = line.split(";");
+          Queue<String> ADTQueueCP;
+          ADTQueueCP = new LinkedList<>();
+          ADTQueueCP.add(s);
+          System.out.println(values[1] + values[2]);
+          return s.split(";");
+         
+
+        
+    } 
+  
+}
+    
+      
+
+  
+  
+    /*try {
+      File myObj = new File("/src/main/java/moniqueproject/computerpionersproject/ComputerPioneers.txt");
+        try (java.util.Scanner myReader = new Scanner(myObj)) {
+            int counter = 1;
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                counter++;
+                System.out.println( counter +data + "");
+            } }
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+    } 
+  }  
     
 
         /* String[] splitLine = line.trim().split(";");
@@ -109,7 +165,7 @@ public static void main(String[] args)throws Exception
         queue.insert(new Data(splitLine[0], splitLine[1], splitLine[2]));
         }
          */
-  }
+  
 
 
   
